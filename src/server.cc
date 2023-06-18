8debf13366ad2d3cc6de9cdbf9dc0bc3104a4f4a
lanthora <lanthora@outlook.com>
2023-06-18 05:45:59 +0000

    支持服务端动态分配 IP 地址

--- src/server.cc
+++ src/server.cc
@@ -41,9 +41,60 @@ int Server::setPassword(std::string password) {
     return 0;
 }
 
+int Server::setDHCP(std::string dhcp) {
+    if (dhcp.empty()) {
+        return 0;
+    }
+
+    std::size_t pos = dhcp.find("/");
+    std::string addr = dhcp.substr(0, pos);
+    _subnet = std::stoi(dhcp.substr(pos + 1));
+    if (_subnet <= 0 || _subnet >= 31 || !INet::isIpv4Address(addr)) {
+        spdlog::critical("dhcp: {0} invalid", dhcp);
+        exit(1);
+    }
+
+    _network = ntohl(INet::ipStringToU32(addr));
+    _network &= ~((1 << (32 - _subnet)) - 1);
+    _lastClientTunIp = _network;
+    return 0;
+}
+
 void Server::handleClientMessage(WebSocket webSocket, const WebSocketMessagePtr &msg) {
 
     if (msg->str.size() < 1) {
+        return;
+    }
+
+    if (msg->str.front() == TYPE_FORWARD) {
+        if (msg->str.size() < sizeof(ForwardHeader)) {
+            return;
+        }
+
+        ForwardHeader *forward = (ForwardHeader *)msg->str.data();
+
+        if (!_ipWsClientMap.contains(forward->iph.saddr)) {
+            return;
+        }
+
+        if (_ipWsClientMap[forward->iph.saddr].expired()) {
+            _ipWsClientMap.erase(forward->iph.saddr);
+            _wsIpClientMap.erase(webSocket);
+            return;
+        }
+
+        if (_ipWsClientMap[forward->iph.saddr].lock() != webSocket.lock()) {
+            return;
+        }
+
+        if (!_ipWsClientMap.contains(forward->iph.daddr)) {
+            return;
+        }
+
+        auto ws = _ipWsClientMap[forward->iph.daddr].lock();
+        if (ws) {
+            ws->sendBinary(msg->str);
+        }
         return;
     }
 
@@ -61,47 +112,84 @@ void Server::handleClientMessage(WebSocket webSocket, const WebSocketMessagePtr 
             return;
         }
 
-        if (_ipWsClientMap.contains(auth->tunIp)) {
-            auto ws = _ipWsClientMap[auth->tunIp].lock();
+        if (_ipWsClientMap.contains(auth->ip)) {
+            auto ws = _ipWsClientMap[auth->ip].lock();
             if (ws) {
                 ws->close();
             }
         }
 
-        _ipWsClientMap[auth->tunIp] = webSocket;
+        _ipWsClientMap[auth->ip] = webSocket;
+        _wsIpClientMap[webSocket] = auth->ip;
         return;
     }
 
-    if (msg->str.front() == TYPE_FORWARD) {
-        if (msg->str.size() < sizeof(ForwardHeader)) {
+    if (msg->str.front() == TYPE_DHCP) {
+        if (!_subnet) {
             return;
         }
 
-        ForwardHeader *forward = (ForwardHeader *)msg->str.data();
-
-        if (!_ipWsClientMap.contains(forward->iph.saddr)) {
+        if (msg->str.size() < sizeof(DHCPHeader)) {
             return;
         }
 
-        if (_ipWsClientMap[forward->iph.saddr].expired()) {
-            _ipWsClientMap.erase(forward->iph.saddr);
+        DHCPHeader *dhcp = (DHCPHeader *)msg->str.data();
+        if (std::abs(unixTimeStamp() - (int64_t)ntohll(dhcp->timestamp)) > 30) {
             return;
         }
 
-        if (_ipWsClientMap[forward->iph.saddr].lock() != webSocket.lock()) {
+        if (!dhcp->checkHash(_password)) {
             return;
         }
 
-        if (!_ipWsClientMap.contains(forward->iph.daddr)) {
-            return;
+        uint32_t nextClientTunIp = _lastClientTunIp;
+        uint32_t mask = (1 << (32 - _subnet)) - 1;
+
+        bool no_address_available = false;
+        while (true) {
+            nextClientTunIp += 1;
+            nextClientTunIp &= mask;
+
+            if (nextClientTunIp == 0) {
+                continue;
+            }
+
+            if (nextClientTunIp == mask) {
+                if (no_address_available) {
+                    spdlog::critical("no address available");
+                    exit(1);
+                }
+                no_address_available = true;
+                continue;
+            }
+
+            if (_ipWsClientMap.contains(htonl(_network | nextClientTunIp))) {
+                continue;
+            }
+
+            _lastClientTunIp = _network | nextClientTunIp;
+            break;
         }
 
-        auto ws = _ipWsClientMap[forward->iph.daddr].lock();
-        if (!ws) {
-            return;
+        std::string ip = INet::ipU32ToString(htonl(_lastClientTunIp));
+        ip += "/";
+        ip += std::to_string(_subnet);
+
+        strcpy(dhcp->cidr, ip.data());
+
+        auto ws = webSocket.lock();
+        if (ws) {
+            ws->sendBinary(msg->str);
         }
-        ws->sendBinary(msg->str);
     }
+}
+
+void Server::handleCloseMessage(WebSocket webSocket, const WebSocketMessagePtr &msg) {
+    auto it = _wsIpClientMap.find(webSocket);
+    if (it != _wsIpClientMap.end()) {
+        _ipWsClientMap.erase(it->second);
+    }
+    _wsIpClientMap.erase(webSocket);
 }
 
 void Server::handleErrorMessage(WebSocket webSocket, const WebSocketMessagePtr &msg) {
@@ -113,6 +201,9 @@ void Server::handleMessage(WebSocket webSocket, ConnectionState connectionState,
     switch (msg->type) {
     case ix::WebSocketMessageType::Message:
         handleClientMessage(webSocket, msg);
+        break;
+    case ix::WebSocketMessageType::Close:
+        handleCloseMessage(webSocket, msg);
         break;
     case ix::WebSocketMessageType::Error:
         handleErrorMessage(webSocket, msg);
