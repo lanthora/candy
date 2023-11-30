91cccdd3098bc42cc18b396a53ac583ca62e5409
lanthora <lanthora@outlook.com>
2023-11-30 04:44:18 +0000

    实现 Windows 主函数

--- src/main/windows.cc
+++ src/main/windows.cc
@@ -2,13 +2,27 @@
 #if defined(_WIN32) || defined(_WIN64)
 
 #include "core/client.h"
+#include "core/server.h"
 #include <condition_variable>
+#include <filesystem>
+#include <fstream>
+#include <libconfig.h++>
 #include <mutex>
 #include <signal.h>
 #include <spdlog/spdlog.h>
 #include <winsock.h>
 
 namespace {
+
+struct arguments {
+    std::string mode;
+    std::string websocket;
+    std::string tun;
+    std::string dhcp;
+    std::string password;
+    std::string name;
+    std::string stun;
+};
 
 bool running = true;
 std::mutex mutex;
@@ -31,6 +45,31 @@ bool windowsNetworkCleanup() {
     return WSACleanup() == 0;
 }
 
+int saveLatestAddress(const std::string &name, const std::string &cidr) {
+    std::string cfgFile = "address/";
+    cfgFile += name.empty() ? "__noname__" : name;
+    std::filesystem::create_directories(std::filesystem::path(cfgFile).parent_path());
+    std::ofstream ofs(cfgFile);
+    if (ofs.is_open()) {
+        ofs << cidr;
+        ofs.close();
+    }
+    return 0;
+}
+
+std::string getLastestAddress(const std::string &name) {
+    std::string cfgFile = "address/";
+    cfgFile += name.empty() ? "__noname__" : name;
+    std::ifstream ifs(cfgFile);
+    if (!ifs.is_open()) {
+        return "";
+    }
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    ifs.close();
+    return ss.str();
+}
+
 } // namespace
 
 namespace Candy {
@@ -39,17 +78,47 @@ void shutdown() {
 }
 } // namespace Candy
 
-// TODO(windows): 实现 Windows 的主函数
 int main() {
     windowsNetworkStartup();
 
-    spdlog::set_level(spdlog::level::debug);
-
+    Candy::Server server;
     Candy::Client client;
-    client.setWebSocketServer("wss://zone.icandy.one/demo");
-    client.setName("demo");
-    client.setStun("stun://stun.qq.com");
-    client.run();
+    struct arguments arguments;
+
+    try {
+        libconfig::Config cfg;
+        cfg.readFile("candy.conf");
+        cfg.lookupValue("mode", arguments.mode);
+        cfg.lookupValue("websocket", arguments.websocket);
+        cfg.lookupValue("tun", arguments.tun);
+        cfg.lookupValue("dhcp", arguments.dhcp);
+        cfg.lookupValue("stun", arguments.stun);
+        cfg.lookupValue("password", arguments.password);
+        cfg.lookupValue("name", arguments.name);
+    } catch (const libconfig::FileIOException &fioex) {
+        spdlog::critical("i/o error while reading configuration file");
+        return 0;
+    } catch (const libconfig::ParseException &pex) {
+        spdlog::critical("parse error at {} : {} - {}", pex.getFile(), pex.getLine(), pex.getError());
+        return 0;
+    }
+
+    if (arguments.mode == "server") {
+        server.setPassword(arguments.password);
+        server.setWebSocketServer(arguments.websocket);
+        server.setDynamicAddressRange(arguments.dhcp);
+        server.run();
+    }
+
+    if (arguments.mode == "client") {
+        client.setPassword(arguments.password);
+        client.setWebSocketServer(arguments.websocket);
+        client.setStun(arguments.stun);
+        client.setLocalAddress(arguments.tun);
+        client.setDynamicAddress(getLastestAddress(arguments.name));
+        client.setName(arguments.name);
+        client.run();
+    }
 
     spdlog::info("service started successfully");
 
@@ -61,7 +130,13 @@ int main() {
         condition.wait(lock, [&] { return !running; });
     }
 
+    server.shutdown();
     client.shutdown();
+
+    if (!client.getAddress().empty()) {
+        saveLatestAddress(arguments.name, client.getAddress());
+    }
+
     windowsNetworkCleanup();
 
     spdlog::info("service stopped successfully");
