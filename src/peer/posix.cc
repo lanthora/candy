2b10a7d67e88ff5dc7ce5190eb91a3ba02052d63
lanthora <lanthora@outlook.com>
2023-12-10 05:12:24 +0000

    减少大流量时冗余 select 造成的性能损失

--- src/peer/posix.cc
+++ src/peer/posix.cc
@@ -17,6 +17,16 @@ UdpHolder::UdpHolder() {
         spdlog::error("create udp socket failed: {}", strerror(errno));
         return;
     }
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0) {
+        spdlog::error("get udp socket flags failed: {}", strerror(errno));
+        return;
+    }
+    flags |= O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, flags) < 0) {
+        spdlog::error("set non-blocking udp socket failed: {}", strerror(errno));
+        return;
+    }
     this->socket = fd;
     return;
 }
@@ -37,35 +47,31 @@ size_t UdpHolder::read(UdpMessage &message) {
         return -1;
     }
 
-    struct timeval timeout = {.tv_sec = 1};
-
-    fd_set set;
-    FD_ZERO(&set);
-    FD_SET(fd, &set);
-
-    int ret = select(fd + 1, &set, NULL, NULL, &timeout);
-    if (ret == 0) {
-        return 0;
-    }
-    if (ret < 0) {
-        spdlog::error("udp socket select failed: error {}", ret);
-        return -1;
-    }
-
     char buffer[1500];
     struct sockaddr_in from;
     socklen_t addr_len = sizeof(from);
     memset(&from, 0, sizeof(from));
 
     ssize_t len = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&from, &addr_len);
-    if (len == -1) {
-        spdlog::error("udp socket read failed: {}", strerror(errno));
-        return -1;
+    if (len >= 0) {
+        message.buffer.assign(buffer, len);
+        message.ip = Address::netToHost(from.sin_addr.s_addr);
+        message.port = Address::netToHost(from.sin_port);
+        return len;
     }
-    message.buffer.assign(buffer, len);
-    message.ip = Address::netToHost(from.sin_addr.s_addr);
-    message.port = Address::netToHost(from.sin_port);
-    return len;
+
+    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        struct timeval timeout = {.tv_sec = 1};
+
+        fd_set set;
+        FD_ZERO(&set);
+        FD_SET(fd, &set);
+
+        select(fd + 1, &set, NULL, NULL, &timeout);
+        return 0;
+    }
+    spdlog::error("udp socket read failed: {}", strerror(errno));
+    return -1;
 }
 
 size_t UdpHolder::write(const UdpMessage &message) {
