cc059d845cf6c08f2d68d27ffd580e3bf39c6c35
lanthora <lanthora@outlook.com>
2024-03-30 04:00:51 +0000

    修复 Windows 导出函数

--- src/cffi/candy.cc
+++ src/cffi/candy.cc
@@ -1,87 +1,12 @@
 // SPDX-License-Identifier: MIT
-#include "cffi/candy.h"
 #include "core/client.h"
 #include <stdlib.h>
 
-void *candy_client_create() {
-    Candy::Client *c = new Candy::Client();
-    return c;
-}
-
-void candy_client_release(void *candy) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    delete c;
-}
-
-int candy_client_set_name(void *candy, const char *name) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setName(name);
-}
-
-int candy_client_set_password(void *candy, const char *password) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setPassword(password);
-}
-
-int candy_client_set_websocket_server(void *candy, const char *server) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setWebSocketServer(server);
-}
-
-int candy_client_set_tun_address(void *candy, const char *cidr) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setTunAddress(cidr);
-}
-
-int candy_client_set_expected_address(void *candy, const char *cidr) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setExpectedAddress(cidr);
-}
-
-int candy_client_set_virtual_mac(void *candy, const char *vmac) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setVirtualMac(vmac);
-}
-
-int candy_client_set_stun(void *candy, const char *stun) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setStun(stun);
-}
-
-int candy_client_set_discovery_interval(void *candy, int interval) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setDiscoveryInterval(interval);
-}
-
-int candy_client_set_route_cost(void *candy, int cost) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setRouteCost(cost);
-}
-
-int candy_client_set_address_update_callback(void *candy, void (*callback)(const char *)) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setAddressUpdateCallback([&](const std::string address) { callback(address.c_str()); });
-}
-
-int candy_client_set_udp_bind_port(void *candy, int port) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setUdpBindPort(port);
-}
-
-int candy_client_set_localhost(void *candy, char *ip) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->setLocalhost(ip);
-}
-
-int candy_client_run(void *candy) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->run();
-}
-
-int candy_client_shutdown(void *candy) {
-    Candy::Client *c = static_cast<Candy::Client *>(candy);
-    return c->shutdown();
-}
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
 
 namespace {
 void (*internal_error_cb)(void *) = NULL;
@@ -97,7 +22,95 @@ void shutdown(Client *c) {
 }
 } // namespace Candy
 
-int candy_client_set_error_cb(void (*callback)(void *)) {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+EXPORT void *candy_client_create() {
+    Candy::Client *c = new Candy::Client();
+    return c;
+}
+
+EXPORT void candy_client_release(void *candy) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    delete c;
+}
+
+EXPORT int candy_client_set_name(void *candy, const char *name) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setName(name);
+}
+
+EXPORT int candy_client_set_password(void *candy, const char *password) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setPassword(password);
+}
+
+EXPORT int candy_client_set_websocket_server(void *candy, const char *server) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setWebSocketServer(server);
+}
+
+EXPORT int candy_client_set_tun_address(void *candy, const char *cidr) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setTunAddress(cidr);
+}
+
+EXPORT int candy_client_set_expected_address(void *candy, const char *cidr) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setExpectedAddress(cidr);
+}
+
+EXPORT int candy_client_set_virtual_mac(void *candy, const char *vmac) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setVirtualMac(vmac);
+}
+
+EXPORT int candy_client_set_stun(void *candy, const char *stun) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setStun(stun);
+}
+
+EXPORT int candy_client_set_discovery_interval(void *candy, int interval) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setDiscoveryInterval(interval);
+}
+
+EXPORT int candy_client_set_route_cost(void *candy, int cost) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setRouteCost(cost);
+}
+
+EXPORT int candy_client_set_address_update_callback(void *candy, void (*callback)(const char *)) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setAddressUpdateCallback([&](const std::string address) { callback(address.c_str()); });
+}
+
+EXPORT int candy_client_set_udp_bind_port(void *candy, int port) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setUdpBindPort(port);
+}
+
+EXPORT int candy_client_set_localhost(void *candy, char *ip) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->setLocalhost(ip);
+}
+
+EXPORT int candy_client_run(void *candy) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->run();
+}
+
+EXPORT int candy_client_shutdown(void *candy) {
+    Candy::Client *c = static_cast<Candy::Client *>(candy);
+    return c->shutdown();
+}
+
+EXPORT int candy_client_set_error_cb(void (*callback)(void *)) {
     internal_error_cb = callback;
     return 0;
 }
+
+#ifdef __cplusplus
+}
+#endif
--- src/cffi/candy.h
+++ src/cffi/candy.h
@@ -2,27 +2,33 @@
 #ifndef CANDY_CFFI_CANDY_H
 #define CANDY_CFFI_CANDY_H
 
+#ifdef _WIN32
+#define IMPORT __declspec(dllimport)
+#else
+#define IMPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void *candy_client_create();
-int candy_client_set_name(void *candy, const char *name);
-int candy_client_set_password(void *candy, const char *password);
-int candy_client_set_websocket_server(void *candy, const char *server);
-int candy_client_set_tun_address(void *candy, const char *cidr);
-int candy_client_set_expected_address(void *candy, const char *cidr);
-int candy_client_set_virtual_mac(void *candy, const char *vmac);
-int candy_client_set_stun(void *candy, const char *stun);
-int candy_client_set_discovery_interval(void *candy, int interval);
-int candy_client_set_route_cost(void *candy, int cost);
-int candy_client_set_udp_bind_port(void *candy, int port);
-int candy_client_set_localhost(void *candy, char *ip);
-int candy_client_set_address_update_callback(void *candy, void (*callback)(const char *));
-int candy_client_set_error_cb(void (*callback)(void *));
-int candy_client_run(void *candy);
-int candy_client_shutdown(void *candy);
-void candy_client_release(void *candy);
+IMPORT void *candy_client_create();
+IMPORT int candy_client_set_name(void *candy, const char *name);
+IMPORT int candy_client_set_password(void *candy, const char *password);
+IMPORT int candy_client_set_websocket_server(void *candy, const char *server);
+IMPORT int candy_client_set_tun_address(void *candy, const char *cidr);
+IMPORT int candy_client_set_expected_address(void *candy, const char *cidr);
+IMPORT int candy_client_set_virtual_mac(void *candy, const char *vmac);
+IMPORT int candy_client_set_stun(void *candy, const char *stun);
+IMPORT int candy_client_set_discovery_interval(void *candy, int interval);
+IMPORT int candy_client_set_route_cost(void *candy, int cost);
+IMPORT int candy_client_set_udp_bind_port(void *candy, int port);
+IMPORT int candy_client_set_localhost(void *candy, char *ip);
+IMPORT int candy_client_set_address_update_callback(void *candy, void (*callback)(const char *));
+IMPORT int candy_client_set_error_cb(void (*callback)(void *));
+IMPORT int candy_client_run(void *candy);
+IMPORT int candy_client_shutdown(void *candy);
+IMPORT void candy_client_release(void *candy);
 
 #ifdef __cplusplus
 }
