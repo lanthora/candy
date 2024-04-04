49161cf98d52ae77b06b869d234c8502c96ee3d6
lanthora <lanthora@outlook.com>
2024-04-04 07:02:38 +0000

    允许作为第三方库引入

--- cmake/libconfig++.cmake
+++
@@ -1,5 +0,0 @@
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(LIBCONFIGXX REQUIRED libconfig++)
-
-include_directories(${LIBCONFIGXX_INCLUDEDIR})
-target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${LIBCONFIGXX_LIBRARIES})
--- cmake/spdlog.cmake
+++
@@ -1,8 +0,0 @@
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(SPDLOG REQUIRED spdlog)
-
-add_definitions(${SPDLOG_CFLAGS})
-
-include_directories(${SPDLOG_INCLUDEDIR})
-target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${SPDLOG_LIBRARIES})
-target_link_libraries(${CANDY_LIBRARY_NAME} PRIVATE ${SPDLOG_LIBRARIES})
