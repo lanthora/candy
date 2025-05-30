d90adfe353a267f483d9aeb8b8a236aa10996e19
lanthora <lanthora@outlook.com>
2025-05-30 14:58:45 +0800

    处理 Wintun 同步问题

--- src/peer/message.h
+++ src/peer/message.h
@@ -3,6 +3,7 @@
 #define CANDY_PEER_MESSAGE_H
 
 #include "core/net.h"
+#include "utils/random.h"
 #include <cstdint>
 
 namespace Candy {
@@ -20,12 +21,18 @@ struct __attribute__((packed)) StunRequest {
     uint8_t type[2] = {0x00, 0x01};
     uint8_t length[2] = {0x00, 0x08};
     uint8_t cookie[4] = {0x21, 0x12, 0xa4, 0x42};
-    uint8_t id[12] = {0x00};
+    uint32_t id[3] = {0x00};
     struct __attribute__((packed)) {
         uint8_t type[2] = {0x00, 0x03};
         uint8_t length[2] = {0x00, 0x04};
         uint8_t notset[4] = {0x00};
     } attr;
+
+    StunRequest() {
+        id[0] = randomUint32();
+        id[1] = randomUint32();
+        id[2] = randomUint32();
+    }
 };
 
 struct __attribute__((packed)) StunResponse {
--- src/tun/tun.cc
+++ src/tun/tun.cc
@@ -15,7 +15,6 @@ int Tun::run(Client *client) {
         while (this->client->running) {
             handleTunQueue();
         }
-        spdlog::debug("tun msg thread exit");
     });
     return 0;
 }
@@ -120,7 +119,6 @@ void Tun::handleTunAddr(Msg msg) {
         while (this->client->running) {
             handleTunDevice();
         }
-        spdlog::debug("tun handle thread exit");
         if (down()) {
             Candy::shutdown(this->client);
             return;
